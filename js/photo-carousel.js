f7d4f6e75cb555408c1ff8427442fa591ae5a7fb
Tamir Sh. <60156530+shtamir@users.noreply.github.com>
2025-05-07 07:46:41 +0300

    Update photo-carousel.js

--- js/photo-carousel.js
+++ js/photo-carousel.js
@@ -17,7 +17,7 @@
 }; */
 
 // Configuration for day-wise and time-wise photo display
-const photoSchedule_regular = {
+const photoSchedule = {
   "Sunday": {
     "morning": ["images/carousel/morning_03.gif", "images/carousel/sunday_01.jpg"],
     "noon": ["images/carousel/goodday_01.jpg", "images/carousel/sunday_01.jpg"],
@@ -132,7 +132,7 @@ photoSchedule_holocaust = {
 };
 
 // photoSchedule_memorial_day
-const photoSchedule = {
+const photoSchedule_memorial_day = {
   "Sunday": {
     "morning": ["images/carousel/morning_03.gif", "images/carousel/sunday_01.jpg"],
     "noon": ["images/carousel/goodday_01.jpg", "images/carousel/sunday_01.jpg"],
